91d7faacffdaf565d06b1156d01520aa48475daa
yazzheka <42967131+Yazzheka@users.noreply.github.com>
2019-01-24 16:19:00 +0100

    Enter key now submits your answer and moves to the next question, only 1 answer can be submitted for each question otherwise an error message will be displayed, if you try to submit an empty string an error message will be displayed. You get points for every correct answer. Current question and total questions now get displayed. When you submit your answer on the last question a scorescreen now appears to let the user show how well he did( NOT FUNCTIONAL YET), When the user closes the scorescreen the quiz gets reset and the user can play again.

--- quiz.js
+++ quiz.js
@@ -1,33 +1,127 @@
-    const quiz = {
+    // Quiz database.
+    var quiz = {
         question:["whats striped black and white and lives in Africa.","What is the primitive ancestor of the human race.","Which animal lives in Africa and uses its long neck to eat leaves from tall trees."],
         answer:["Zebra","Monkeys","Giraffe"],
     };
+    // UserInput database.
+    var userAnswer = [[],[],[]];
+    // Initializing variables.
     let questionDisplay = document.getElementById("question-display");
     let questionCount = document.getElementById("question-count");
     let points = document.getElementById("points");
     let input = document.getElementById("input");
     let submit = document.getElementById("submit");
+    let modalScoreScreen = document.getElementById("modal-container");
+    let modalBody = document.getElementById("modal-body");
+    let closeModal = document.getElementById("close-modal");
     let nextButton = document.getElementById("right-arrow");
     let backButton = document.getElementById("left-arrow");
+    let body = document.getElementsByTagName("body");
+    var tempArr;
+    
     
     // num is the currentQuestion index.
-    let num = 0;
+    var num = 0;
+    var numArr = -1;
+    var totalPoints = 0;
+    // Initializing Buttons.
     backButton.addEventListener("click", back);
     nextButton.addEventListener("click", next);
+    submit.addEventListener("click", storeUserAnswer);
+    input.addEventListener("keyup", function(event){
+        event.preventDefault();
+        if(event.keyCode === 13){
+            submit.click();
+            
+        }
+    });
+    closeModal.addEventListener("click", closeScoreScreen);
+    window.addEventListener("keyup", function(event){
+        event.preventDefault();
+        if(event.keyCode === 27){
+            closeModal.click();
+        }
+    });
     // Where the quiz starts.
 function play(){
     questionDisplay.innerHTML = quiz.question[0];
+    questionCount.innerHTML = num +1 + "/" + quiz.question.length;
+    //var userAnswer = [[],[],[]];
+    //return userAnswer;
     
 };
     // Moves to the next question.
 function next(){
-    num++;
-    questionDisplay.innerHTML = quiz.question[num];
+    if(num == quiz.question.length - 1){
+        return false;
+    }
+    numArr++
+    num++;console.log(num);
+    
+    questionDisplay.innerHTML = quiz.question[num];questionCount.innerHTML = num + 1 + "/" + quiz.question.length;
+    return num;
 }
     // Moves to the previous question.
 function back(){
-    num--;
-    questionDisplay.innerHTML = quiz.question[num];
+    if(num <= 0){
+        return false;
+    }
+    numArr--
+    num--;console.log(num);
+    
+    questionDisplay.innerHTML = quiz.question[num];questionCount.innerHTML = num + 1 + "/" + quiz.question.length;
 }
+    // takes the users answer and pushes it to userAnswer database.
+function storeUserAnswer(){
+    let last = userAnswer.length -1;
+    
+    if(userAnswer[num].length  >= 1 ){
+        alert("you already gave your answer, please go to the next question.");
+        input.value = "";
+        return false;
+    }
+    if(input.value == ""){
+        alert("ERROR you need to enter something");
+        return false;
+    }
+    console.log(userAnswer);
+    userAnswer[num].push(input.value);
+    input.value = "";
+    compareAnswer();
+    next();
+    console.log(userAnswer[last]);
+    if(userAnswer[last].length == 1){
+        scoreScreen();
+    }
+}
+    // check if userInput == quiz.answer
+function compareAnswer(){
+    if(userAnswer[num] == quiz.answer[num]){
+        totalPoints += 100;
+        points.innerHTML = totalPoints;
+    }
+}
+    // Resets the Quiz.
+function replay(){
+    userAnswer = [[],[],[]];
+    num = 0;
+    totalPoints = 0;
+    console.log(userAnswer);
+    questionDisplay.innerHTML = quiz.question[0];
+    console.log(questionDisplay.innerHTML = quiz.question[0]);
+    questionCount.innerHTML = num +1+ "/" + quiz.question.length;
+    points.innerHTML = totalPoints;
+    console.log(questionCount.innerHTML = num +1+ "/" + quiz.question.length);
+}
+    // Scorescreen that is shown at the end of the game.
+function scoreScreen(){
+    modalScoreScreen.style.display = "block";
+    replay();
+}
+function closeScoreScreen(){
+    modalScoreScreen.style.display = "none";
+}
+    // Ends the game.
+function endGame(){}
 // start the quiz.
 play();
